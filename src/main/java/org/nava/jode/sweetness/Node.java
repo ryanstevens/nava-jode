58f4f2387363046a16ec93f4ca549f421cbbcd27
Ryan Stevens <stevens.ryan@gmail.com>
2014-12-05 13:26:20 -0800

    initial commit

---
+++ src/main/java/org/nava/jode/sweetness/Node.java
@@ -0,0 +1,10 @@
+package org.nava.jode.sweetness;
+
+public class Node {
+
+  public static void main(String [ ] args) {
+    System.out.println("console.log('Hello world from the JVM');");  
+  
+  }
+
+} 
